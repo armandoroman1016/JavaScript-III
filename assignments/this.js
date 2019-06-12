0a72e1a038bdbf7a71073e04e685d82fed821e42
Armando Roman <armandoroman1016@gmail.com>
2019-06-12 13:07:57 -0700

    4 principles in this.js complete

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,13 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Principle one is the window/object binder. When we call on 'this' in the global scope we will get returned back to us everything Javascript gives us access to.
+
+* 2. Implicit binding is when we use 'this' to call on our functions. This will refer to the Object that precedes the 'dot'.
+
+* 3. The third principle 'New' is used when we want to create a new class using a constructor. We use the 'new' keyword to tie the new object to the constructor.
+
+* 4. Explicit binding is some we used when we want to change or override a constructors values. We use Call and Apply to give 'this' object new values. 
 *
 * write out a code example of each explanation above
 */
